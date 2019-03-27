5828060b6c6a5d0f1c03d6cb54acc8b365058899
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-03-27 16:43:23 -0400

    First upload to new project folder.

---
+++ javascripts/app.js
@@ -0,0 +1,65 @@
+// Rover Object Goes Here
+// ======================
+var rover = {
+  direction:"N",
+  x:0,
+  y:0,
+}
+// ======================
+function turnLeft(rover){
+  switch(rover.direction) {
+      case "N":
+      rover.direction="W";
+      break;
+    case "E":
+      rover.direction="N";
+      break;
+    case "S":
+      rover.direction="E";
+      break;
+    case "W":
+      rover.direction="S"
+      break;
+    }
+console.log("turnLeft was called!");
+}
+
+function turnRight(rover){ 
+  switch(rover.direction) {
+    case "N":
+      rover.direction="E";
+      break;
+    case "E":
+      rover.direction="S";
+      break;
+    case "S":
+      rover.direction="W";
+      break;
+    case "W":
+      rover.direction="N";
+      break;
+    }
+  console.log("turnRight was called!");
+}
+
+function moveForward(rover) {
+  switch (rover) {
+    case "N":
+      rover.y=rover.y--;
+    break;
+    case "S":
+      rover.y=rover.y++;
+    break;
+    case "E":
+      rover.x=rover.x++;
+    break;
+    case "W":
+      rover.x=rover.x--;
+    break;  
+  }
+  console.log("moveForward was called")
+}
+
+moveForward(rover);
+console.log("The Rover's direction is now: " + rover.direction);
+console.log("The Rover is now at position: " + rover.x + "," + rover.y);
